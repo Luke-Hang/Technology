a20e5e50b7d9fe7625573ce41fe790bda0718402
xiehangz <694851182@qq.com>
2026-02-24 23:58:55 +0800

    1

--- src/main/java/com/cashgenerator/interceptor/JwtTokenFilter.java
+++ src/main/java/com/cashgenerator/interceptor/JwtTokenFilter.java
@@ -26,6 +26,8 @@ public class JwtTokenFilter extends OncePerRequestFilter {
             return;
         }
 
+
+
         //验证 token 有效性
         String token = header.split(" ")[1].trim();
         if (!jwtUtils.validateJwtToken(token)) {
